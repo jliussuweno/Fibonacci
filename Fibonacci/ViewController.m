415d9cafe28f936df82cd1198ec0b14454940c03
Jlius Suweno <jliuss@live.com>
2020-10-12 21:59:01 +0700

    Update ViewController.m

--- Fibonacci/ViewController.m
+++ Fibonacci/ViewController.m
@@ -15,7 +15,32 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self displayFibonacci:10];
+}
+
+- (void)displayFibonacciNumber:(int) number {
+    NSMutableArray *fibSeries = [NSMutableArray new];
+    int total = 0;
+    int prev = 1;
+    for (int i = 0 ; i < number; i++) {
+        total = total + prev;
+        prev = total - prev;
+        [fibSeries addObject:[NSNumber numberWithInt:total]];
+    }
+    NSLog(@"Fibonacci number are: %@", fibSeries);
+}
+
+- (void)displayFibonacci:(int) number {
+    int fn = 1;
+    int fn1 = 1;
+    int fn2 = 0;
+    
+    for(int i = 1; i <= number; i++) {
+        NSLog(@"Fibonacci angka ke %d adalah %d", i, fn);
+        fn = fn2 + fn1;
+        fn2 = fn1;
+        fn1 = fn;
+    }
 }
 
 
